90328ad58cd4808ba84e841e35effa2a540e200d
alirfanyasin <risayunita21@gmail.com>
2024-05-08 11:03:43 +0700

    feat: footer

---
+++ resources/views/livewire/components/footer.blade.php
@@ -0,0 +1,99 @@
+<footer class="py-5 light-cream-bg">
+  <div class="container py-5">
+    <div class="row mb-5">
+      <div class="col-md-3">
+        <img src="/mov-assets/logo/logo.png" alt="Logo" class="mb-4">
+        <p class="fw-light">Jl. Ketintang Barat III No.71 A, Ketintang, Gayungan, Surabaya, Jawa Timur 60231</p>
+        <div class="icon mt-5 d-flex">
+          <div class="d-flex white-bg border justify-content-center align-items-center rounded-pill"
+            style="width: 40px; height: 40px;">
+            <iconify-icon icon="bi:twitter"></iconify-icon>
+          </div>
+          <div class="d-flex text-white black-bg mx-3 justify-content-center align-items-center rounded-pill"
+            style="width: 40px; height: 40px;">
+            <iconify-icon icon="ri:facebook-fill"></iconify-icon>
+          </div>
+          <div class="d-flex white-bg border justify-content-center align-items-center rounded-pill"
+            style="width: 40px; height: 40px;">
+            <iconify-icon icon="mdi:instagram"></iconify-icon>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 d-flex justify-content-center">
+        <div>
+          <header>
+            <h5 class="fw-semibold text-uppercase">Company</h5>
+          </header>
+
+          <ul class="ps-0">
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">About</a>
+            </li>
+            <li class="list-unstyled"><a href=""
+                class="lh-lg text-decoration-none text-dark fw-light">Features</a>
+            </li>
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">Works</a>
+            </li>
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">Career</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 d-flex justify-content-center">
+        <div>
+          <header>
+            <h5 class="fw-semibold text-uppercase">Layanan Konsumen</h5>
+          </header>
+
+          <ul class="ps-0">
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">Layanan
+                Pengaduan Konsumen</a>
+            </li>
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">FAQ</a>
+            </li>
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">Kebijakan
+                Privasi</a>
+            </li>
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">Syarat dan
+                Ketentuan</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 d-flex justify-content-end">
+        <div>
+          <header>
+            <h5 class="fw-semibold text-uppercase">Hubungi Kami</h5>
+          </header>
+
+          <ul class="ps-0">
+            <li class="list-unstyled"><a href=""
+                class="lh-lg text-decoration-none text-dark fw-light">Whatsapp</a>
+            </li>
+            <li class="list-unstyled"><a href=""
+                class="lh-lg text-decoration-none text-dark fw-light">+6287863299291</a>
+            </li>
+            <li class="list-unstyled"><a href="" class="lh-lg text-decoration-none text-dark fw-light">Email</a>
+            </li>
+            <li class="list-unstyled"><a href=""
+                class="lh-lg text-decoration-none text-dark fw-light">hello@mov.co.id</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+      <div class="col-md-8">
+        &copy; 2024, PT Basis Inovasi Global
+      </div>
+      <div class="col-md-4 d-flex justify-content-end">
+        <img src="/mov-assets/logo/payment.png" alt="Payment">
+      </div>
+    </div>
+
+  </div>
+
+</footer>
