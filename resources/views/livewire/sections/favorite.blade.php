d4ae162556718a8b477be1aba6a1cb9ab03c3bd1
alirfanyasin <risayunita21@gmail.com>
2024-05-08 08:32:56 +0700

    feat: from favorite to news-skincare section

---
+++ resources/views/livewire/sections/favorite.blade.php
@@ -0,0 +1,144 @@
+<section id="favorite-section" class="my-5">
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <header class="text-center my-5">
+          <h2 class="fw-light text-dark text-uppercase">Favorit Pelanggan</h2>
+        </header>
+      </div>
+    </div>
+
+
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card border-0 rounded-4 shadow overflow-hidden position-relative">
+          <div class="card-body">
+            <div class="mb-3 rounded-3">
+              <img src="/mov-assets/product/img-product-4.png" alt="Product 1" width="100%">
+            </div>
+            <h6 class="fw-bold">Serum Vitamin C</h6>
+            <div id="" class="d-flex align-items-center">
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+
+              <iconify-icon icon="material-symbols:star-half-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="ic:round-star-border" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <small class="text-main fs-10  ms-2">5.0/5</small>
+            </div>
+            <div class="fw-bold mt-2 d-flex align-items-center">Rp. 85.000 &nbsp; <span
+                class="px-2 py-1 orange-color fw-light rounded-pill discount fs-10">20%</span></div>
+          </div>
+          <div class="position-absolute top-0" style="right: 40px;">
+            <img src="/mov-assets/shape/label-sale.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-0 rounded-4 shadow overflow-hidden position-relative">
+          <div class="card-body">
+            <div class="mb-3 rounded-3">
+              <img src="/mov-assets/product/img-product-4.png" alt="Product 1" width="100%">
+            </div>
+            <h6 class="fw-bold">Serum Vitamin C</h6>
+            <div id="" class="d-flex align-items-center">
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+
+              <iconify-icon icon="material-symbols:star-half-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="ic:round-star-border" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <small class="text-main fs-10  ms-2">5.0/5</small>
+            </div>
+            <div class="fw-bold mt-2 d-flex align-items-center">Rp. 85.000 &nbsp; <span
+                class="px-2 py-1 orange-color fw-light rounded-pill discount fs-10">20%</span></div>
+          </div>
+          <div class="position-absolute top-0" style="right: 40px;">
+            <img src="/mov-assets/shape/label-sale.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-0 rounded-4 shadow overflow-hidden position-relative">
+          <div class="card-body">
+            <div class="mb-3 rounded-3">
+              <img src="/mov-assets/product/img-product-4.png" alt="Product 1" width="100%">
+            </div>
+            <h6 class="fw-bold">Serum Vitamin C</h6>
+            <div id="" class="d-flex align-items-center">
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+
+              <iconify-icon icon="material-symbols:star-half-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="ic:round-star-border" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <small class="text-main fs-10  ms-2">5.0/5</small>
+            </div>
+            <div class="fw-bold mt-2 d-flex align-items-center">Rp. 85.000 &nbsp; <span
+                class="px-2 py-1 orange-color fw-light rounded-pill discount fs-10">20%</span></div>
+          </div>
+          <div class="position-absolute top-0" style="right: 40px;">
+            <img src="/mov-assets/shape/label-sale.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-0 rounded-4 shadow overflow-hidden position-relative">
+          <div class="card-body">
+            <div class="mb-3 rounded-3">
+              <img src="/mov-assets/product/img-product-4.png" alt="Product 1" width="100%">
+            </div>
+            <h6 class="fw-bold">Serum Vitamin C</h6>
+            <div id="" class="d-flex align-items-center">
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="material-symbols:star-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+
+              <iconify-icon icon="material-symbols:star-half-rounded" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <iconify-icon icon="ic:round-star-border" width="1em" height="1em"
+                class="text-warning"></iconify-icon>
+              <small class="text-main fs-10  ms-2">5.0/5</small>
+            </div>
+            <div class="fw-bold mt-2 d-flex align-items-center">Rp. 85.000 &nbsp; <span
+                class="px-2 py-1 orange-color fw-light rounded-pill discount fs-10">20%</span></div>
+          </div>
+          <div class="position-absolute top-0" style="right: 40px;">
+            <img src="/mov-assets/shape/label-sale.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <div class="d-flex justify-content-center">
+          <a href="" wire:navigate
+            class="text-decoration-none green-color white-bg shadow py-2 px-5 d-inline-block mt-5 rounded-pill fw-semibold">Produk
+            Lainnya</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
