b35e45fced7d379d6b5f5e4d66f459256e002e33
alirfanyasin <risayunita21@gmail.com>
2024-05-08 22:01:33 +0700

    fix: checkout page

--- resources/views/livewire/pages/cart.blade.php
+++ resources/views/livewire/pages/cart.blade.php
@@ -12,7 +12,7 @@
     <div class="container">
       <div class="row d-flex align-items-center">
         <div class="col-1 align-self-start">
-          <iconify-icon icon="mdi:location"></iconify-icon>
+          <iconify-icon icon="mdi:location" class="fs-5"></iconify-icon>
         </div>
         <div class="col-md-6 ">
           <h5 class="fw-semibold">Alamat Pengiriman</h5>
@@ -37,77 +37,180 @@
 
   {{-- Product start --}}
   <div class="container mb-5">
-    <div class="row my-2">
+    <div class="row">
+      <div class="col-1"></div>
       <div class="col">
-        <div class="d-flex justify-content-between align-items-center ">
-          <div class="d-flex">
-            <div class="overflow-hidden rounded-4 border " style="width: 130px;">
-              <img src="/mov-assets/product/img-product-1.png" alt="" width="100%">
-            </div>
-            <div class="ms-4">
-              <h4 class="fw-bold">Serum Vitamin C</h5>
-                <h5>Rp. 102.000</h5>
-                <h5>1x</h5>
+        <div class="row my-2">
+          <div class="col">
+            <div class="d-flex justify-content-between align-items-center ">
+              <div class="d-flex">
+                <div class="overflow-hidden rounded-4 border " style="width: 130px;">
+                  <img src="/mov-assets/product/img-product-1.png" alt="" width="100%">
+                </div>
+                <div class="ms-4">
+                  <h4 class="fw-bold">Serum Vitamin C</h5>
+                    <h5>Rp. 102.000</h5>
+                    <h5>1x</h5>
+                </div>
+              </div>
+              <div>
+                <a href="" class="text-decoration-none text-danger">
+                  <iconify-icon icon="humbleicons:times" class="fs-30"></iconify-icon>
+                </a>
+              </div>
             </div>
           </div>
-          <div>
-            <a href="" class="text-decoration-none text-danger">
-              <iconify-icon icon="humbleicons:times" class="fs-30"></iconify-icon>
-            </a>
+        </div>
+        <hr>
+        <div class="row my-2">
+          <div class="col">
+            <div class="d-flex justify-content-between align-items-center ">
+              <div class="d-flex">
+                <div class="overflow-hidden rounded-4 border " style="width: 130px;">
+                  <img src="/mov-assets/product/img-product-1.png" alt="" width="100%">
+                </div>
+                <div class="ms-4">
+                  <h4 class="fw-bold">Serum Vitamin C</h5>
+                    <h5>Rp. 102.000</h5>
+                    <h5>1x</h5>
+                </div>
+              </div>
+              <div>
+                <a href="" class="text-decoration-none text-danger">
+                  <iconify-icon icon="humbleicons:times" class="fs-30"></iconify-icon>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="row my-2">
+          <div class="col">
+            <div class="d-flex justify-content-between align-items-center ">
+              <div class="d-flex">
+                <div class="overflow-hidden rounded-4 border " style="width: 130px;">
+                  <img src="/mov-assets/product/img-product-1.png" alt="" width="100%">
+                </div>
+                <div class="ms-4">
+                  <h4 class="fw-bold">Serum Vitamin C</h5>
+                    <h5>Rp. 102.000</h5>
+                    <h5>1x</h5>
+                </div>
+              </div>
+              <div>
+                <a href="" class="text-decoration-none text-danger">
+                  <iconify-icon icon="humbleicons:times" class="fs-30"></iconify-icon>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <hr>
-    <div class="row my-2">
-      <div class="col">
-        <div class="d-flex justify-content-between align-items-center ">
-          <div class="d-flex">
-            <div class="overflow-hidden rounded-4 border " style="width: 130px;">
-              <img src="/mov-assets/product/img-product-1.png" alt="" width="100%">
+
+  </div>
+  {{-- Product end --}}
+
+  {{-- Delivery Option start --}}
+  <div class="op-green-bg py-4 border border-black border-start-0 border-end-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col">
+          <div class="row">
+            <div class="col">
+              <div class="d-flex justify-content-between align-items-center">
+                <div>
+                  <h5 class="fw-bold">Opsi Pengiriman</h5>
+                  <div class="fs-5">Reguler ( 2 - 3 Hari )</div>
+                  <div class="fs-5">Estimasi tiba : 2 - 4 Mei</div>
+                </div>
+                <div>
+                  <span class="fs-5">Rp. 20.000</span>
+                </div>
+              </div>
             </div>
-            <div class="ms-4">
-              <h4 class="fw-bold">Serum Vitamin C</h5>
-                <h5>Rp. 102.000</h5>
-                <h5>1x</h5>
-            </div>
-          </div>
-          <div>
-            <a href="" class="text-decoration-none text-danger">
-              <iconify-icon icon="humbleicons:times" class="fs-30"></iconify-icon>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <hr>
-    <div class="row my-2">
-      <div class="col">
-        <div class="d-flex justify-content-between align-items-center ">
-          <div class="d-flex">
-            <div class="overflow-hidden rounded-4 border " style="width: 130px;">
-              <img src="/mov-assets/product/img-product-1.png" alt="" width="100%">
-            </div>
-            <div class="ms-4">
-              <h4 class="fw-bold">Serum Vitamin C</h5>
-                <h5>Rp. 102.000</h5>
-                <h5>1x</h5>
-            </div>
-          </div>
-          <div>
-            <a href="" class="text-decoration-none text-danger">
-              <iconify-icon icon="humbleicons:times" class="fs-30"></iconify-icon>
-            </a>
           </div>
         </div>
       </div>
     </div>
   </div>
-  {{-- Product end --}}
-
-  {{-- Delivery Option start --}}
-
   {{-- Delivery Option end --}}
+
+  {{-- Message start --}}
+  <div class="container py-1 mt-3" id="msg-cart">
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col d-flex justify-content-between">
+        <label for="" class="fs-5">Pesan :</label>
+        <input type="text" name="" id="" class="border-0 shadow-none"
+          placeholder="Silahkan tinggalkan pesan...">
+      </div>
+    </div>
+  </div>
+  {{-- Message end --}}
+
+  <hr>
+  {{-- Payment Method start --}}
+  <div class="container py-1 mt-3" id="msg-cart">
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col">
+        <div class="row">
+          <div class="col d-flex justify-content-between">
+            <h5>Total Pesanan (1 Produk) : </h5>
+            <span class="fs-5 dark-cream-color fw-bold">Rp 102.000</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-1">
+        <iconify-icon icon="solar:dollar-bold" class="fs-5"></iconify-icon>
+      </div>
+      <div class="col d-flex justify-content-between align-items-center">
+        <h5 class="fw-bold">Metode Pembayaran</h5>
+        <span>Transfer Bank - Bank BCA</span>
+      </div>
+    </div>
+  </div>
+  {{-- Payment Method end --}}
+  <hr>
+  {{-- Payment list start --}}
+  <div class="container py-1 mt-3" id="msg-cart">
+    <div class="row">
+      <div class="col-1">
+        <iconify-icon icon="solar:bill-list-bold" class="fs-5"></iconify-icon>
+      </div>
+      <div class="col">
+        <h5 class="fw-bold">Rincian Pembayaaran</h5>
+
+        <div class="d-flex justify-content-between ">
+          <div class="fs-5">Subtotal Produk</div>
+          <div class="fs-5">Rp 102.000</div>
+        </div>
+        <div class="d-flex justify-content-between ">
+          <div class="fs-5">Subtotal Pengiriman</div>
+          <div class="fs-5">Rp 20.000</div>
+        </div>
+        <div class="d-flex justify-content-between ">
+          <h5 class="fs-5 fw-bold">Total Pembayaran</h5>
+          <div class="fs-5 dark-cream-color fw-bold">Rp 122.000</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  {{-- Payment list end --}}
+  <hr>
+
+  <div class="container mb-5">
+    <div class="row">
+      <div class="col d-flex justify-content-end">
+        <button class="dark-cream-bg text-white border-0 fw-bold px-3 py-2 rounded-2">Buat Pesanan</button>
+      </div>
+    </div>
+  </div>
 
 
 </div>
