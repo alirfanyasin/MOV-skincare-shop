c4d70da22da31115edc2437e4c5780e8ccafacc9
alirfanyasin <risayunita21@gmail.com>
2024-05-09 11:32:30 +0700

    feat: skintest old page

--- resources/views/livewire/pages/skintest/about-self2.blade.php
+++ resources/views/livewire/pages/skintest/about-self2.blade.php
@@ -1,10 +1,23 @@
 <div class="container">
-  <div class="question text-center mt-5 mb-3">
+  <div class="question text-center mt-5 mb-5">
     <h1 class="fw-bold">Berapa usia kamu?</h1>
   </div>
   <form action="">
     <div class="row d-flex justify-content-center">
-
+      <div class="col-md-6">
+        <div class="row d-flex justify-content-center">
+          <div class="col-md-6 mb-4">
+            <button class="dark-cream-bg text-white border-2 w-100 py-4 rounded-2">10 - 18 Tahun</button>
+          </div>
+          <div class="col-md-6 mb-4">
+            <button class="dark-cream-bg text-white border-2 w-100 py-4 rounded-2">19 - 25 Tahun</button>
+          </div>
+          <div class="col-md-6 mb-4">
+            <button class="dark-cream-bg text-white border-2 w-100 py-4 rounded-2">26 - 45 Tahun</button>
+          </div>
+        </div>
+        <p class="text-center">Tidak perlu malu... dengan umurmu membantu kami lebih memahami tentang kulitmu.</p>
+      </div>
     </div>
   </form>
 </div>
