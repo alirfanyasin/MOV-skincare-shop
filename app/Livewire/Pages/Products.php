860304f3c1cd43c9b2ff19261c046ca5677f63af
alirfanyasin <risayunita21@gmail.com>
2024-06-08 21:40:13 +0700

    get product

--- app/Livewire/Pages/Products.php
+++ app/Livewire/Pages/Products.php
@@ -2,8 +2,7 @@
 
 namespace App\Livewire\Pages;
 
-use Closure;
-use Illuminate\Contracts\View\View;
+use App\Models\Product;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -13,8 +12,10 @@ class Products extends Component
     #[Title('Products')]
     #[Layout('layouts.product-layout')]
 
-    public function render(): View|Closure|string
+    public function render()
     {
-        return view('livewire.pages.products');
+        return view('livewire.pages.products', [
+            'products' => Product::orderBy('id', 'DESC')->get()
+        ]);
     }
 }
